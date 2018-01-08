b1a169148f91a6c12d9490f6b5212241fed3b392
RoArriaza <ro.arriazasilva@gmail.com>
2018-01-08 14:57:54 -0300

    etapa9 CHECK

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -7,17 +7,19 @@ console.log("Hola!")
 
   $(".js-show-recipe").click(function(){
     $("#divMake").removeClass("make");
-
+    $(this).addClass("active");
+    $(".js-show-make").removeClass("active");
 
   });
-
 
 
   $(".js-show-make").click(function(){
     $("#divMake").addClass("make");
-
-
+    $(this).addClass("active");
+    $(".js-show-recipe").removeClass("active");
   });
+
+
 
 
 });
