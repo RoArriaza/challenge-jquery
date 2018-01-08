f1c40fa1591ec0550332537031b5931b23124bb7
RoArriaza <ro.arriazasilva@gmail.com>
2018-01-08 14:48:37 -0300

    etapa 8 CHECK

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,10 +3,22 @@ $(document).ready(function(){
   $(".js-menu").css("display", "none");
   $(".js-back").css("display", "block");
 
-
-
 console.log("Hola!")
 
+  $(".js-show-recipe").click(function(){
+    $("#divMake").removeClass("make");
+
+
+  });
+
+
+
+  $(".js-show-make").click(function(){
+    $("#divMake").addClass("make");
+
+
+  });
 
 
 });
+
